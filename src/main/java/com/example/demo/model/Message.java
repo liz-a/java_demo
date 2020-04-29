7f2a23571e1f08f71446052d74b9bd9e32b709bc
“Liz <liz.ardolino@hotmail.com>
2020-04-29 15:55:59 +0100

    post req

--- src/main/java/com/example/demo/model/Message.java
+++ src/main/java/com/example/demo/model/Message.java
@@ -3,6 +3,9 @@ package com.example.demo.model;
 public class Message {
   private String content;
 
+  public Message() {
+  }
+
   public Message(String content) {
     this.content = content;
   }
